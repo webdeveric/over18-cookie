f08380ad3e2536899aceea1e64acff09ea42e1c2
Eric King <eric@webdeveric.com>
2020-10-06 09:01:10 -0500

    Set cookies in all stores

--- src/background.js
+++ src/background.js
@@ -2,22 +2,21 @@
  * @param {string} url
  * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/cookies/set
  */
-async function setOver18Cookie( url )
+async function setOver18Cookie({ url, domain, firstPartyDomain, storeId })
 {
   try {
-    const domain = '.reddit.com';
     const firstPartyIsolate = await browser.privacy.websites.firstPartyIsolate.get({});
-    const firstPartyDomain = firstPartyIsolate.value ? 'reddit.com' : '';
 
     const cookie = await browser.cookies.set({
       name: 'over18',
       value: '1',
       url,
       domain,
-      firstPartyDomain,
+      firstPartyDomain: firstPartyIsolate.value ? firstPartyDomain : '',
       path: '/',
       httpOnly: false,
       secure: true,
+      storeId,
     });
 
     return cookie;
@@ -28,26 +27,63 @@ async function setOver18Cookie( url )
   }
 }
 
-async function init()
+async function setCookies()
 {
-  await Promise.allSettled([
-    setOver18Cookie('https://www.reddit.com/'),
-    setOver18Cookie('https://old.reddit.com/'),
-  ]);
+  const stores = await browser.cookies.getAllCookieStores();
+
+  const promises = [];
+
+  const domainDetails = {
+    domain: '.reddit.com',
+    firstPartyDomain: 'reddit.com',
+  };
+
+  const urls = [
+    'https://www.reddit.com/',
+    'https://old.reddit.com/',
+  ];
+
+  stores.forEach( store => {
+    urls.forEach( url => {
+      promises.push(
+        setOver18Cookie({
+          ...domainDetails,
+          url,
+          storeId: store.id,
+        }),
+      );
+    });
+  });
+
+  const results = await Promise.allSettled( promises );
+
+  return results;
 }
 
 function onFirstPartyIsolateChange({ value })
 {
   console.log(`privacy.firstparty.isolate has been changed to ${value}`);
 
-  init();
+  setCookies();
+}
+
+function onWindowCreated(/* window */)
+{
+  setCookies();
 }
 
 /**
- * Firefox does not fire this event from manual changes in about:conig
+ * Firefox does not fire this event from manual changes in about:config
  *
  * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/types/BrowserSetting/onChange
  */
 browser.privacy.websites.firstPartyIsolate.onChange.addListener( onFirstPartyIsolateChange );
 
-init();
+/**
+ * This will not fire for incognito windows unless "Run in Private Windows" is set to "Allow"
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/windows/onCreated
+ */
+browser.windows.onCreated.addListener( onWindowCreated );
+
+setCookies();
