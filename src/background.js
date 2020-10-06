366ead2d311d32bb2c5a841404c432cfc6358c1d
Eric King <eric@webdeveric.com>
2020-10-06 01:06:08 -0500

    Initial commit

---
+++ src/background.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} url
+ * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/cookies/set
+ */
+async function setOver18Cookie( url )
+{
+  try {
+    const domain = '.reddit.com';
+    const firstPartyIsolate = await browser.privacy.websites.firstPartyIsolate.get({});
+    const firstPartyDomain = firstPartyIsolate.value ? 'reddit.com' : '';
+
+    const cookie = await browser.cookies.set({
+      name: 'over18',
+      value: '1',
+      url,
+      domain,
+      firstPartyDomain,
+      path: '/',
+      httpOnly: false,
+      secure: true,
+    });
+
+    return cookie;
+  } catch (error) {
+    console.error( error );
+
+    throw error;
+  }
+}
+
+async function init()
+{
+  await Promise.allSettled([
+    setOver18Cookie('https://www.reddit.com/'),
+    setOver18Cookie('https://old.reddit.com/'),
+  ]);
+}
+
+function onFirstPartyIsolateChange({ value })
+{
+  console.log(`privacy.firstparty.isolate has been changed to ${value}`);
+
+  init();
+}
+
+/**
+ * Firefox does not fire this event from manual changes in about:conig
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/types/BrowserSetting/onChange
+ */
+browser.privacy.websites.firstPartyIsolate.onChange.addListener( onFirstPartyIsolateChange );
+
+init();
---
+++ web-ext-config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  verbose: true,
+  sourceDir: path.join(__dirname, 'src'),
+  artifactsDir: path.join(__dirname, 'build'),
+  build: {
+    overwriteDest: true,
+  },
+  run: {
+    startUrl: [
+      'about:devtools-toolbox?type=extension&id=over18-cookie%40webdeveric.com',
+      'about:config',
+    ],
+  },
+  ignoreFiles: [ 'package-lock.json' ],
+};
